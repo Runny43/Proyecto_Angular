e864baaf56cd590019e2c22b5128b45a6ff921d6
MMR <rmm@gmail.com>
2025-07-30 12:00:46 -0600

    Creacion de controladores importantes

---
+++ Backend/Sistema de citas/Sistema de citas/Controllers/QuotesController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sistema_de_citas.DatabaseHelper;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Sistema_de_citas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuotesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public QuotesController(AppDbContext context)
+
+        {
+
+            _context = context;
+
+        }
+
+        // GET: api/<QuotesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<QuotesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<QuotesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<QuotesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<QuotesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
