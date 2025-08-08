bd13a466c8f0e5e90d79b5ce5d1e8bb1175a31b9
Victor Núñez <victornunezarias763@gmail.com>
2025-08-08 15:42:57 -0600

    cambios del login y register

--- Frontend/Sistema_De_Citas/src/app/login/login.ts
+++ Frontend/Sistema_De_Citas/src/app/login/login.ts
@@ -2,11 +2,13 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {
   FormBuilder,
+  FormControl,
+  FormGroup,
   FormsModule,
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
-import { UserService, User } from '../services/user-service';
+import { UserService } from '../services/user-service';
 import { Router, RouterLink } from '@angular/router';
  
 @Component({
@@ -22,14 +24,17 @@ export class Login {
   private router = inject(Router);
  
   newUser = this.fb.group({
-    user_name: ['', Validators.required],
     email: ['', [Validators.required, Validators.email]],
     password: ['', Validators.required],
-    user_role: ['', Validators.required],
   });
  
+
   LoginUser() {
-    this.api.Login(this.newUser.value).subscribe({
+    const { email, password } = this.newUser.value;
+    this.api.Login({
+      email: email ?? '',
+      password: password ?? ''
+    }).subscribe({
       next: (res) => {
         alert('sign in');
         this.router.navigate(['/']);
